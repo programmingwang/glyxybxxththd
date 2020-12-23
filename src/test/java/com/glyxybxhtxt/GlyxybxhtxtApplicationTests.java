c1ef6c51a6f9b3aaf7bee8882959ddbc79ac71ff
programmingwang <64132721+programmingwang@users.noreply.github.com>
2020-12-23 11:40:51 +0800

    12.23 数据库连接时区问题

--- src/test/java/com/glyxybxhtxt/GlyxybxhtxtApplicationTests.java
+++ src/test/java/com/glyxybxhtxt/GlyxybxhtxtApplicationTests.java
@@ -1,13 +1,15 @@
-package com.glyxybxhtxt;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GlyxybxhtxtApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.glyxybxhtxt;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//
+//@SpringBootTest
+//class GlyxybxhtxtApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//
+//    }
+//
+//}
